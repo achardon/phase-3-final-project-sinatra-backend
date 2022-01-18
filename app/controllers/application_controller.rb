60431c360f0df7198aff1243c7076b9843545cde
Alessandra Chardon <achardon@ucla.edu>
2022-01-18 16:04:47 -0500

    add delete bike rides endpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
@@ -9,6 +11,23 @@ class ApplicationController < Sinatra::Base
   get "/bike_rides" do
     bike_rides = BikeRide.all
     bike_rides.to_json
+  end
+
+  post "/bike_rides" do
+    # binding.pry
+    bike_ride = BikeRide.create(
+      name: params[:name],
+      date: params[:date],
+      description: params[:description],
+      rider_id: params[:rider_id],
+      route_id: params[:route_id]
+    )
+    bike_ride.to_json
+  end
+
+  delete "/bike_rides/:id" do
+    bike_ride = BikeRide.find(params[:id])
+    bike_ride.destroy
   end
 
   get "/riders" do
